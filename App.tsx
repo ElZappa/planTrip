5519f691b60bf0d9b93231dc37983723db99d7a4
mzappacosta <mzappacosta@ivolutia.com>
2023-11-15 04:13:52 +0100

    upgrade navbar to fixed position

--- App.tsx
+++ App.tsx
@@ -1,41 +1,48 @@
 import React from "react";
 import "./App.css";
-import ResponsiveNavbar from "./components/navbar/Navbar";
-import { Grid } from "@mui/material";
+import NavbarComponent from "./components/navbar/NavbarComponent";
+import { Box, CssBaseline, Grid } from "@mui/material";
 import ItemCard from "./components/cardTrip/itemCard";
+// import Box from "@mui/material/Box";
 
 function App() {
   return (
-    <div>
-      <ResponsiveNavbar />
-      <Grid
-        container
-        spacing={2}
-        mt={0}
-        ml={0}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <NavbarComponent />
+        <Grid
+          container
+          spacing={2}
+          mt={7}
+          ml={0}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          {/* <Grid> //Activar spinner de carga cuando se llame al servior
+            <CircularProgress />
+          </Grid> */}
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} xl={2}>
+            <ItemCard />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} xl={2}>
-          <ItemCard />
-        </Grid>
-      </Grid>
-    </div>
+      </Box>
+    </>
   );
 }
 
